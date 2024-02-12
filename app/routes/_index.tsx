9f1b5890987620d3b69530c15f23603b9dc72be6
AmitYoffe <yoffe.amit@gmail.com>
2024-02-12 15:44:13 +0200

    Dark mode button

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,17 +1,18 @@
 import type { MetaFunction } from "@remix-run/node";
+import { ModeToggle } from "~/components/mode-toggle";
 import { Button } from "~/components/ui/button";
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+  return [{ title: "PassCode inference" }];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <Button>askjdhas</Button>
+    <div className="grid grid-cols-[30%_40%_30%] grid-rows-[20%_80%] h-screen gap-1	p-2">
+      <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+        <Button>Some random button that doesnt do anything!</Button>
+        <ModeToggle />
+      </div>
     </div>
   );
 }
