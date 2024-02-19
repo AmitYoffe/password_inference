79a96b520c28e620303e2194a5f2854c07e83ef5
AmitYoffe <yoffe.amit@gmail.com>
2024-02-19 21:53:36 +0200

    added charts

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,6 +2,7 @@ import type { MetaFunction } from "@remix-run/node";
 import Keypad from "~/components/keypadComps/Keypad";
 import { ModeToggle } from "~/components/mode-toggle";
 import { Button } from "~/components/ui/button";
+import { Chart } from "~/components/ui/graph";
 
 export const meta: MetaFunction = () => {
   return [{ title: "PassCode inference" }];
@@ -11,7 +12,7 @@ export default function Index(): JSX.Element {
   return (
     <div className="h-screen gap-1 p-2 grid grid-cols-[30%_40%_30%]">
       <div className="">
-        <Button>Some random button that doesnt do anything!</Button>
+        <Chart />
       </div>
       <div className="flex items-center justify-center">
         <Keypad />
